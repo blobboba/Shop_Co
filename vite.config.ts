7a36612aa09153c9cf2cc83ef023dbec3714478e
blobboba <158776912+blobboba@users.noreply.github.com>
2025-05-13 19:50:28 +0400

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/Shop_Co/',  
+  base: '/Shop_Co/', 
   plugins: [react()],
 })
