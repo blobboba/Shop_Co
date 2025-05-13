f25883cd95d4b0f9f427456d75ad2b946926ec73
blobboba <158776912+blobboba@users.noreply.github.com>
2025-05-13 19:49:21 +0400

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/Shop_Co/',  // Указание правильного пути для GitHub Pages
+  base: '/Shop_Co/',  
   plugins: [react()],
 })
